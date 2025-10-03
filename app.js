4fb17edfcab204ae88fc697b9f8c49ea68579b79
Phan Phu <phanphu399@gmail.com>
2025-10-03 16:15:53 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const contactRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
+
 const app = express();
 
 app.use(cors());
@@ -11,4 +13,15 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/contacts", contactRouter);
+
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal server error",
+  });
+});
+
 module.exports = app;
